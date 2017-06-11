2d51c0f1ba48e9da362d5adadefb484202cb6396
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2017-06-11 15:28:51 +0430

    Add project files.

---
+++ LEARNING_CONCEPTS/Father.cs
@@ -0,0 +1,36 @@
+﻿namespace LEARNING_CONCEPTS
+{
+	// **************************************************
+	//public class Father : object
+	//{
+	//	public Father() : base()
+	//	{
+	//	}
+
+	//	public void Work()
+	//	{
+	//		System.Windows.Forms.MessageBox.Show
+	//			("از ساعت هشت صبح تا چهار بعد از ظهر کفاشی می‌کنم");
+	//	}
+	//}
+	// **************************************************
+
+	// **************************************************
+	public class Father : object
+	{
+		public Father() : base()
+		{
+		}
+
+		/// <summary>
+		/// یعنی فرزند من می‌تواند همان کار مرا داشته باشد
+		/// و یا کار تکمیلی و یا حتی دیگری را پیشه کند
+		/// </summary>
+		public virtual void Work()
+		{
+			System.Windows.Forms.MessageBox.Show
+				("از ساعت هشت صبح تا چهار بعد از ظهر کفاشی می‌کنم");
+		}
+	}
+	// **************************************************
+}
---
+++ LEARNING_CONCEPTS/MainForm.cs
@@ -0,0 +1,21 @@
+﻿namespace LEARNING_CONCEPTS
+{
+	public partial class MainForm : System.Windows.Forms.Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void MainForm_Load(object sender, System.EventArgs e)
+		{
+			//Father oFather = new Father();
+
+			//oFather.Work();
+
+			Son oSon = new Son();
+
+			oSon.Work();
+		}
+	}
+}
